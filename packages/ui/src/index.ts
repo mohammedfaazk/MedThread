199dbbdd5fa4deb9dfa684231efe4a185aace47c
navinnaz <navinnazerine.26csb@licet.ac.in>
2026-02-11 19:13:33 +0530

    removing-old

--- packages/ui/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './components/PostCard';
-export * from './components/ThreadReply';
-export * from './components/SymptomTag';
-export * from './components/DoctorBadge';
-export * from './theme';
